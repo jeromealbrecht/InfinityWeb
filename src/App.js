5a3d0004b1b80769b7359b02263e70fac0716d6c
December81 <megastudio@outlook.fr>
2023-10-30 21:36:15 +0100

    Stage changes

--- src/App.js
+++ src/App.js
@@ -5,24 +5,32 @@ import './AMedia-queries.css';
 
 const App = () => {
   const [showContent, setShowContent] = useState(false);
+  const [showMirrorImage, setShowMirrorImage] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const element = document.querySelector('.scroll-content');
       if (element) {
         const elementPosition = element.getBoundingClientRect();
-        if (elementPosition.top <= window.innerHeight && !showContent) {
+        const activationPoint = window.innerHeight * 0.5; // Ajustez la fraction selon vos besoins
+  
+        if (elementPosition.top <= activationPoint && !showContent) {
           setShowContent(true);
+        }
+  
+        // Check if the user has scrolled down after 'description-text' is displayed.
+        if (showContent && window.scrollY > elementPosition.bottom) {
+          setShowMirrorImage(true);
         }
       }
     };
-
+  
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [showContent]);
-console.log(showContent);
+
   return (
     <div className="background">
       <nav className="container-nav">
@@ -31,20 +39,16 @@ console.log(showContent);
       </nav>
       <div className="content">
         <h1 className="main-title">
-        NOUS SOMMES YOLO <br />
-        UNE AGENCE DIGITALE QUI <br />
-        RÉVOLUTIONNE L'EXCEPTIONNEL
+          NOUS SOMMES YOLO <br />
+          UNE AGENCE DIGITALE QUI <br />
+          RÉVOLUTIONNE L'EXCEPTIONNEL
         </h1>
+
+        {/* Scroll buttons */}
         <div className={`scroll-content ${showContent && 'show'}`}>
           <p className='description-text'>
-          À l'ère du numérique, nous façonnons la confiance avec l'excellence.
+            À l'ère du numérique, nous façonnons la confiance avec l'excellence.
           </p>
-          {/* <div className="buttons">
-            <button className="preorder-button">SITES WEB</button>
-            <button className="preorder-button">DESIGN</button>
-            <button className="preorder-button">RÉFÉRENCEMENT</button>
-            <button className="preorder-button">SÉCURITÉ</button>
-          </div> */}
 
           <div className='all-buttons'>
             <div className="buttons">
@@ -55,7 +59,6 @@ console.log(showContent);
               <button className="preorder-button">RÉFÉRENCEMENT</button>
               <button className="preorder-button">SÉCURITÉ</button>
             </div>
-
           </div>
 
           <div className="description">
@@ -64,6 +67,30 @@ console.log(showContent);
             are designed to provide the most strength, durability, and comfort possible.
           </div>
         </div>
+
+        {/* Mirror Image */}
+        {showMirrorImage && (
+        <div className={`scroll-content show`}>
+          <div className={`mirror-image ${showMirrorImage ? 'show' : ''}`}>
+            <div className="description-transition">
+              <p className='description-text lh'>
+                Notre Expertise Web <br /> au service de Couleur de Son
+              </p>
+
+              <p className='description-text-small'>
+              Le site web Couleur de Son est bien plus qu'une simple vitrine. <br /><br />
+              Il est le reflet de son image, de leur engagement auprès des clients. <br /><br />
+              Notre expertise web a permis de mettre en avant la qualité des services proposés, <br /><br />
+              de faciliter la navigation des visiteurs et d'améliorer la visibilité en ligne.
+              </p>
+            </div>
+            <div className='image-w'>
+              <div className={`image-transition ${showMirrorImage ? 'show' : ''}`}></div>
+            </div>
+
+          </div>
+        </div>
+      )}
       </div>
     </div>
   );
